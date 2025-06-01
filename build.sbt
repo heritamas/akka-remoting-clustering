66f84d8801e5b789a47e8416d73b8be4867566b4
Heri Tamas <heri.tamas@gmail.com>
2025-06-01 16:52:07 +0200

    resolving remote actors

--- build.sbt
+++ build.sbt
@@ -4,13 +4,17 @@ version := "0.1"
 
 scalaVersion := "2.13.14"
 
-lazy val akkaVersion = "2.8.8"
+lazy val akkaVersion = "2.6.20"
 lazy val protobufVersion = "3.6.1"
+lazy val aeronVersion = "1.47.5"
+
+javaOptions += "--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-remote" % akkaVersion,
   "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
   "com.typesafe.akka" %% "akka-cluster-sharding" % akkaVersion,
-  "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion
+  "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
+  "io.aeron"           % "aeron-driver" % aeronVersion
 )
--- src/main/scala/part2_remoting/RemoteActors.scala
+++ src/main/scala/part2_remoting/RemoteActors.scala
@@ -1,6 +1,6 @@
 package part2_remoting
 
-import akka.actor.{Actor, ActorIdentity, ActorLogging, ActorRef, ActorSystem, Identify, Props}
+import akka.actor.{Actor, ActorIdentity, ActorLogging, ActorRef, ActorSystem, Identify, Props, Stash}
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 
@@ -12,22 +12,22 @@ object RemoteActors extends App {
 
   val localSystem = ActorSystem("LocalSystem", ConfigFactory.load("part2_remoting/remoteActors.conf"))
   val localSimpleActor = localSystem.actorOf(Props[SimpleActor], "localSimpleActor")
-  localSimpleActor ! "hello, local actor!"
+  //localSimpleActor ! "hello, local actor!"
 
   // send a message to the REMOTE simple actor
 
   // Method 1: actor selection
-  val remoteActorSelection = localSystem.actorSelection("akka://RemoteSystem@localhost:2552/user/remoteSimpleActor")
-  remoteActorSelection ! "hello from the \"local\" JVM"
+  //val remoteActorSelection = localSystem.actorSelection("akka://RemoteSystem@localhost:2552/user/remoteSimpleActor")
+  //remoteActorSelection ! "hello from the \"local\" JVM"
 
   // Method 2: resolve the actor selection to an actor ref
-  import localSystem.dispatcher
-  implicit val timeout = Timeout(3 seconds)
-  val remoteActorRefFuture = remoteActorSelection.resolveOne()
-  remoteActorRefFuture.onComplete {
-    case Success(actorRef) => actorRef ! "I've resolved you in a future!"
-    case Failure(ex) => println(s"I failed to resolve the remote actor because: $ex")
-  }
+//  import localSystem.dispatcher
+//  implicit val timeout = Timeout(3 seconds)
+//  val remoteActorRefFuture = remoteActorSelection.resolveOne()
+//  remoteActorRefFuture.onComplete {
+//    case Success(actorRef) => actorRef ! "I've resolved you in a future!"
+//    case Failure(ex) => println(s"I failed to resolve the remote actor because: $ex")
+//  }
 
   // Method 3: actor identification via messages
   /*
@@ -36,7 +36,7 @@ object RemoteActors extends App {
     - the remote actor will AUTOMATICALLY respond with ActorIdentity(42, actorRef)
     - the actor resolver is free to use the remote actorRef
    */
-  class ActorResolver extends Actor with ActorLogging {
+  class ActorResolver extends Actor with ActorLogging with Stash {
     override def preStart(): Unit = {
       val selection = context.actorSelection("akka://RemoteSystem@localhost:2552/user/remoteSimpleActor")
       selection ! Identify(42)
@@ -45,13 +45,38 @@ object RemoteActors extends App {
     override def receive: Receive = {
       case ActorIdentity(42, Some(actorRef)) =>
         actorRef ! "Thank you for identifying yourself!"
+        unstashAll()
+        context.become(forwarder(actorRef))
+      case _ =>
+        // stash all other messages until we receive the ActorIdentity message
+        log.warning("Received an unexpected message before identifying the remote actor. Stashing it.")
+        stash()
+    }
+
+    private def forwarder(actorRef: ActorRef) : Receive = {
+      case message =>
+        log.info(s"Forwarding message '$message' to $actorRef")
+        actorRef forward message
     }
   }
 
-  localSystem.actorOf(Props[ActorResolver], "localActorResolver")
+  val stub = localSystem.actorOf(Props[ActorResolver], "localActorResolver")
+  stub ! "I reach you via the local actor resolver!"
+  stub ! "Another message for the remote actor!"
 }
 
 object RemoteActors_Remote extends App {
+  val remoteSystem = ActorSystem("RemoteSystem", ConfigFactory.load("part2_remoting/remoteActors.conf").getConfig("remoteSystem"))
+  val remoteSimpleActor = remoteSystem.actorOf(Props[SimpleActor], "remoteSimpleActor")
+  remoteSimpleActor ! "hello, remote actor!"
+}
+
+// two actor systems, one local and one remote, both running in the same JVM
+object RemoteActors_Both extends App {
+  val localSystem = ActorSystem("LocalSystem", ConfigFactory.load("part2_remoting/remoteActors.conf"))
+  val localSimpleActor = localSystem.actorOf(Props[SimpleActor], "localSimpleActor")
+  localSimpleActor ! "hello, local actor!"
+
   val remoteSystem = ActorSystem("RemoteSystem", ConfigFactory.load("part2_remoting/remoteActors.conf").getConfig("remoteSystem"))
   val remoteSimpleActor = remoteSystem.actorOf(Props[SimpleActor], "remoteSimpleActor")
   remoteSimpleActor ! "hello, remote actor!"
